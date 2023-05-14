1099454ee8e18e007f03157f6d8d30ab8f61f7e6
ahilacondo <ahilacondo@unsa.edu.pe>
2023-05-13 20:22:37 -0500

    Creando funcion 'sub'

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,21 @@
 
 public class Calculator{
  public static void main(String[] args){
+  //Ejemplo
+
+  //Variables
+  int a= 10;
+  int b= 20;
+
+  //Operaciones
+
+  //SUB
+  int sub= sub(a, b);
+  System.out.println(sub);
   
+ }
+
+ public static sub(int a, int b){
+  return a - b;
  }
 }

86bc5616952139974f4eba2698a665557fa13303
André Hilacondo <116049544+ahilacondo@users.noreply.github.com>
2023-05-15 16:27:31 -0500

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -3,18 +3,12 @@ public class Calculator{
  public static void main(String[] args){
   
  }
-<<<<<<< HEAD
  //Se creo metodo mod
  public static int mod(int a,int b){
     return a%b;   
  }
-
-
-
-=======
-
+ //Se creo metodo sub
  public static int sub(int a, int b){
   return a - b;
  }
->>>>>>> rama-sub
 }
